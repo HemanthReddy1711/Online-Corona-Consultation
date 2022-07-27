76cd0b76447641d746049d23872da3b677f42d4b
nandini.tomar <nandini.tomar@globallogic.com>
2022-07-27 16:39:20 +0530

    doctor and feedback added

---
+++ Online Consultation/Models/Doctor.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Online_Consultation.Models
+{
+    public class Doctor
+    {
+        public int id { get; set; }
+        [DisplayName("Doctor Name")]
+
+        public string Docname { get; set; }
+        public string speciality { get; set; }
+        public string Email{ get; set; }
+        public int fees { get; set; }
+    }
+    public class Patient
+    {
+        public int id { get; set; }
+    }
+
+    public class Feedback
+    {
+        public int id { get; set; }
+        [ForeignKey("patient")]
+        public int pid { get; set; }
+        public Patient patient{ get; set; }
+        public string Description { get; set; }
+    }
+    
+}
