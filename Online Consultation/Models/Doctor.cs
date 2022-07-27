8d83d9fb070a289886fdefa12908cf344bc4282b
HemanthReddy1711 <reddy.reddy@globallogic.com>
2022-07-27 17:36:13 +0530

    Completed almost all the models creation

--- Online Consultation/Models/Doctor.cs
+++ Online Consultation/Models/Doctor.cs
@@ -3,17 +3,29 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Online_Consultation.Models
 {
-    public class Doctor
+    public class DoctorProfile
     {
         public int id { get; set; }
         [DisplayName("Doctor Name")]
 
+        [NotMapped]
+        public IFormFile dImage { get; set; }
+
+        public string docImageUrl { get; set; }
         public string Docname { get; set; }
         public string speciality { get; set; }
         public string Email{ get; set; }
         public int fees { get; set; }
-        
+        [DisplayName("Available Slots")]
+        public string avail { get; set; }
+
     }
+
+
+    //public class appointment
+    //{
+
+    //}
 
     public class Medicine
     {
@@ -25,8 +37,6 @@ namespace Online_Consultation.Models
         public DateTime mfg { get; set; }
 
         public DateTime exp { get; set; }
-
-
     }
     public class Department
     {
@@ -44,14 +54,18 @@ namespace Online_Consultation.Models
 
         public int DepId { get; set; }
         public Department department { get; set; }
-
-
     }
     
-    public class Patient
+    public class PatientProfile
     {
         public int id { get; set; }
-        public string Pname { get; set; }
+        public string pname { get; set; }
+
+        [NotMapped]
+        public IFormFile pImage { get; set; }
+
+        public string pImageUrl { get; set; }
+
         public string address { get; set; }
         public string Mobile{ get; set; }
 
@@ -63,12 +77,12 @@ namespace Online_Consultation.Models
 
         [ForeignKey("patient")]
         public int pid { get; set; }
-        public Patient patient { get; set; }
+        public PatientProfile patient { get; set; }
 
         [ForeignKey("doctor")]
 
         public int did { get; set; }
-        public Doctor doctor { get; set; }
+        public DoctorProfile doctor { get; set; }
 
         [ForeignKey("medicine")]
 
@@ -77,15 +91,50 @@ namespace Online_Consultation.Models
 
     }
     
-    
+    // appointment
 
     public class Feedback
     {
         public int id { get; set; }
         [ForeignKey("patient")]
         public int pid { get; set; }
-        public Patient patient{ get; set; }
+        public PatientProfile patient{ get; set; }
         public string Description { get; set; }
+
+        public byte rating { get; set; }
+        public DateTime feedbackTime { get; set; }
+    }
+
+    public class Service
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string description { get; set; }
+    }
+
+    public class Billing
+    {
+        public int id { get; set; }
+
+        public DateTime billingdate { get; set; }
+        [ForeignKey("medicine")]
+        public int mid { get; set; }
+
+        public Medicine medicine { get; set; }
+
+        [ForeignKey("patient")]
+        public int pid { get; set; }
+        public PatientProfile patient { get; set; }
+
+        [ForeignKey("doctor")]
+
+        public int did { get; set; }
+        public DoctorProfile doctor { get; set; }
+
+        public int totalFee { get; set; }
+
     }
     
 }
