bcf6e7b78f044f88e509068c72abad6b1c2f2b14
HemanthReddy1711 <reddy.reddy@globallogic.com>
2022-07-27 15:24:03 +0530

    First branch

--- Online Consultation/Program.cs
+++ Online Consultation/Program.cs
@@ -11,7 +11,6 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
