19e2ca2079aed899b703fa2e19a680b23f1b5562
sakshi.3 <sakshi.3@globallogic.com>
2022-07-29 00:14:00 +0530

    Branch

---
+++ Online Consultation/Controllers/EmployeesController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Online_Consultation.Models;
+
+namespace Online_Consultation.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public DoctorDbContext doctorDbContext;
+
+        public EmployeesController(DoctorDbContext doctorDbContext)
+        {
+            this.doctorDbContext = doctorDbContext;
+           
+        }
+        public IActionResult Index()
+        {
+
+            return View(doctorDbContext.Employees.ToList());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee employeeProfile)
+        {
+
+            doctorDbContext.Add(employeeProfile);
+            doctorDbContext.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Edit(int? id)
+        {
+            var employees=doctorDbContext.Employees.FirstOrDefault(e =>e.id==id);
+            return View(employees);
+        }
+        [HttpPost]
+        public IActionResult Edit(Employee employeeProfile)
+        {
+            doctorDbContext.Update(employeeProfile);
+            doctorDbContext.SaveChanges();
+            return RedirectToAction(nameof(Index));
+
+        }
+        public IActionResult Details(int? id)
+        {
+            return View(doctorDbContext.Employees.FirstOrDefault(e => e.id == id));
+            
+        }
+        public IActionResult Delete(int? id)
+        {
+            return View(doctorDbContext.Employees.FirstOrDefault(e => e.id == id));
+        }
+        [HttpPost]
+        public IActionResult Delete(Employee employeeProfile)
+        {
+            doctorDbContext.Remove(employeeProfile);
+            doctorDbContext.SaveChanges();
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
+
+    }
+}
