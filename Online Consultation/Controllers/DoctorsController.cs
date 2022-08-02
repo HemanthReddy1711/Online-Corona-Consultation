91f02b4e9491e00f49e02ce966e68efb5752fcf4
sakshi.3 <sakshi.3@globallogic.com>
2022-08-02 13:11:04 +0530

    Authentication remove

--- Online Consultation/Controllers/DoctorsController.cs
+++ Online Consultation/Controllers/DoctorsController.cs
@@ -17,7 +17,7 @@ namespace Online_Consultation.Controllers
             this.doctorDbContext = doctorDbContext;
             this._env = env;
         }
-        [Authorize(Roles ="Admin")]
+       
         public IActionResult Index()
         {
             return View(doctorDbContext.doctorsProfiles.ToList());
