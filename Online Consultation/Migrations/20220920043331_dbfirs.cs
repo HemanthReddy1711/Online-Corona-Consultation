57c399f5a7aad9be60298cfbba6fb66e40a87bb4
HemanthReddy1711 <reddy.reddy@globallogic.com>
2022-09-20 10:09:02 +0530

    done some changes in DB

---
+++ Online Consultation/Migrations/20220920043331_dbfirs.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Online_Consultation.Migrations
+{
+    public partial class dbfirs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
